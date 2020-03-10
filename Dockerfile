414ff170de3465490c533eadae6a66551950e1e0
Valentyn Fiedietsov <fedetsov@gmail.com>
2020-03-10 16:51:18 +0200

    testing configuration varriants

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM alpine:3.10
-COPY entrypoint.sh /entrypoint.sh
+COPY setup-slack-env.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- index.js
+++
@@ -1,5 +0,0 @@
-try {
-  console.log(`::set-env name=SLACK_USERNAME::GH Action - Build`);
-} catch (error) {
-    core.setFailed(error.message);
-  }
