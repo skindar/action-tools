1561dc67876fe5f4e1ee49b1778ef375fb6644b2
Valentyn Fiedietsov <fedetsov@gmail.com>
2020-03-10 16:40:02 +0200

    testing configuration varriants

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 echo ::set-env name=SLACK_USERNAME::GH Action - Build
 echo ::set-env name=SLACK_ICON::https://octodex.github.com/images/jetpacktocat.png
-echo ::set-env name=SLACK_MESSAGE_SUCCESS::${{ github.repository }} build: Success :the_horns:
-echo ::set-env name=SLACK_MESSAGE_STARTED::${{ github.repository }} build: Started :clapper:
-echo ::set-env name=SLACK_MESSAGE_CANCELLED::${{ github.repository }} build: Cancelled: :eyes:
-echo ::set-env name=SLACK_MESSAGE_FAILED::${{ github.repository }} build: Failure: :boom:
+echo ::set-env name=SLACK_MESSAGE_SUCCESS::$1 build: Success :the_horns:
+echo ::set-env name=SLACK_MESSAGE_STARTED::$1 build: Started :clapper:
+echo ::set-env name=SLACK_MESSAGE_CANCELLED::$1 build: Cancelled: :eyes:
+echo ::set-env name=SLACK_MESSAGE_FAILED::$1 build: Failure: :boom:
 
